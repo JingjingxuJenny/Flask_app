d9aa4720210f34e7703b3994e8d9f056cb61d3c0
Coxious <coxious@coxious.com>
2017-03-31 22:57:26 +0800

    Page switch finsihed.

--- __init__.py
+++ __init__.py
@@ -1,20 +1,27 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+import json
 
 @app.route('/')
 def homepage():
     return app.send_static_file("vue_js.html")
 
-
-
 @app.route('/css/index.css')
 def get_CSS():
     return app.send_static_file("/static/css/index.css")
 
-@app.route('/data')
+@app.route('/pages')
 def data():
-    return app.send_static_file("home.html")
+    returned_object = {}
+
+    with open('static/home.html') as home:
+        returned_object['home'] = "".join(home.readlines())
+
+    with open('static/school.html') as home:
+        returned_object['school'] = "".join(home.readlines())
+
+    return json.dumps(returned_object)
 
 if __name__ == "__main__":
     app.run()
